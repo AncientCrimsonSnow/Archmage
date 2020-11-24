0d735978013075541ce48a366f7ddbd9bb0c3c9d
MaxxiP <s0546567@htw-berlin.de>
2020-11-24 08:54:44 +0100

    Changed playermovement and added shooting, the character was swapped to a top down view

--- ge-roguelike-game-project/Assets/Scripts/PlayerMovement.cs
+++ ge-roguelike-game-project/Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,11 @@ public class PlayerMovement : MonoBehaviour
     public float movespeed = 5f;
 
     public Rigidbody2D rb;
-    public Animator animator;
+    //public Animator animator;
+    public Camera cam;
 
     Vector2 movement;
+    Vector2 mousePos;
 
     // Update is called once per frame
     void Update()
@@ -18,14 +20,29 @@ public class PlayerMovement : MonoBehaviour
         // Input
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Horizontal", movement.x);
-        animator.SetFloat("Vertical", movement.y);
-        animator.SetFloat("Speed", movement.sqrMagnitude);
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+        //animator.SetFloat("Horizontal", movement.x);
+        //animator.SetFloat("Vertical", movement.y);
+        //animator.SetFloat("Speed", movement.sqrMagnitude);
+
     }
 
     void FixedUpdate()
     {
+        //Learned almost everything from Brackeys Tutorials
         // Movement
         rb.MovePosition(rb.position + movement * movespeed * Time.fixedDeltaTime);
+        //Debug.Log("Player: " + rb.position);
+        //Debug.Log("Mouse: " + mousePos);
+
+        // Rotation
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg;
+
+        // Debug.Log("Angle: " + angle);
+        // Debug.Log("RB Facing: " +  transform.rotation.eulerAngles.y);
+
+        rb.rotation = angle;
     }
 }
