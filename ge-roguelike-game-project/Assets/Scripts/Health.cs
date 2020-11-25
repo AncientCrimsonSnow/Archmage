15a05b8317043a78f607a1360c687071e7e59688
Daria Katysheva <dariakatysheva92@gmail.com>
2020-11-25 23:11:16 +0100

    Add Health UI

---
+++ ge-roguelike-game-project/Assets/Scripts/Health.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public int health;
+    public int numOfHearts;
+
+    [SerializeField]
+    public Image[] heartsImgs;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    public void HandleHearts()
+    {
+        // If the health is more than number of hearts display max number of hearts
+        if (health > numOfHearts)
+        {
+            health = numOfHearts;
+        }
+
+        for (int i = 0; i < heartsImgs.Length; i++)
+        {
+            if (i < health)
+            {
+                heartsImgs[i].sprite = fullHeart;
+            }
+            else
+            {
+                heartsImgs[i].sprite = emptyHeart;
+            }
+
+            if (i < numOfHearts)
+            {
+                heartsImgs[i].enabled = true;
+            }
+            else
+            {
+                heartsImgs[i].enabled = false;
+            }
+        }
+    }
+}
---
+++ ge-roguelike-game-project/Assets/Scripts/UIManager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+	void Update()
+    {
+		UpdateLives();
+	}
+
+    public void UpdateLives() {
+		FindObjectOfType<Health>().HandleHearts();
+	}
+}
