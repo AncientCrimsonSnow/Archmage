c195f103d9f437514e3c721373d36ede29346222
MaxxiP <s0546567@htw-berlin.de>
2020-11-27 23:08:37 +0100

    Starting a new branch for enemy player interaction

--- ge-roguelike-game-project/Assets/Scripts/Projektile/Bullet.cs
+++ ge-roguelike-game-project/Assets/Scripts/Projektile/Bullet.cs
@@ -5,12 +5,9 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
 
-    public GameObject hitEffect;
 
     void OnCollisionEnter2D(Collision2D collision)
     {
-        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
-        Destroy(effect, 1f);
         Destroy(gameObject);
     }
 }
