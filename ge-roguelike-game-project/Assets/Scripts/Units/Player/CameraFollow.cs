f22f29903da7f2f09f0884d7252b75c2266e6816
MaxxiP <s0546567@htw-berlin.de>
2020-11-28 13:12:18 +0100

    Added Camera Follow Script to center the Player on the screen and zoom

---
+++ ge-roguelike-game-project/Assets/Scripts/Units/Player/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Camera cam;
+
+    private float camHeight = 2.3f;
+    //public float smoothSpeed = 0.5f;
+    private Vector3 offset = new Vector3(0, 0, -10);
+
+    void LateUpdate(){
+
+        transform.position = target.position + offset;
+        cam.orthographicSize = camHeight;
+    }
+}
