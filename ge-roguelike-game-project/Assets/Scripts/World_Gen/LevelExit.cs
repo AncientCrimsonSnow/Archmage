6937614e2d89302a5a4595c03dee8f32f5910eb6
Daria Katysheva <dariakatysheva92@gmail.com>
2020-11-28 22:51:28 +0100

    Load next level after bumping into the boss

---
+++ ge-roguelike-game-project/Assets/Scripts/World_Gen/LevelExit.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField]
+    private float levelLoadDelay = 2f;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+
+        var currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (Application.CanStreamedLevelBeLoaded(currentSceneIndex + 1))
+        {
+            SceneManager.LoadScene(currentSceneIndex + 1);
+        }
+        else
+        {
+            Debug.Log("Load Scene failed.");
+        }
+    }
+}
