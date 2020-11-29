510ffa60b26dcc0fb3e5766d5ea9b0144caf6276
Daria Katysheva <dariakatysheva92@gmail.com>
2020-11-29 18:50:11 +0100

    Add current level counter to the UI

---
+++ ge-roguelike-game-project/Assets/Scripts/UI/LevelCounter.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelCounter : MonoBehaviour
+{
+    public Text currentLevelText;
+
+    internal void SetGameLevel(int level)
+    {
+        currentLevelText.text = "Current level: " + level;
+    }
+}
