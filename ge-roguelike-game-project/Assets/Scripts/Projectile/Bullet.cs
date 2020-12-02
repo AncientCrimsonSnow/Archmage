2a68d7e886c4156526af474f1dbf2b6983c803c5
MaxxiP <s0546567@htw-berlin.de>
2020-12-02 18:36:32 +0100

    Added damage interaction with boss and other enemies, as well as pathfinding for enemies

--- ge-roguelike-game-project/Assets/Scripts/Projectile/Bullet.cs
+++ ge-roguelike-game-project/Assets/Scripts/Projectile/Bullet.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-
-
-
-    void OnCollisionEnter2D(Collision2D collision)
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        Destroy(gameObject);
+        int damageAmount = 10;
+
+        if (collision.CompareTag("Player"))
+        {
+
+            Debug.Log("I hit " + collision.gameObject.tag + " and would deal no Damage!!!");
+
+        }else if (collision.CompareTag("Enemy"))
+        {
+            Enemy enemy = collision.GetComponent<Enemy>();
+
+            if(enemy != null){
+                enemy.takeDamage(damageAmount);
+                Destroy(gameObject);
+            }
+
+        }else if(collision.CompareTag("Wall")){
+            Destroy(gameObject);
+        }
     }
 }
