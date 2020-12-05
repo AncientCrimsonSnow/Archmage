db7c12509bd65fbda13d99ae45648f5d57966639
MaxxiP <s0546567@htw-berlin.de>
2020-12-05 14:24:06 +0100

    Added a new wall sprite and fixed some enemy behaviour

--- ge-roguelike-game-project/Assets/Scripts/Projectile/Bullet.cs
+++ ge-roguelike-game-project/Assets/Scripts/Projectile/Bullet.cs
@@ -7,14 +7,19 @@ public class Bullet : MonoBehaviour
     void OnTriggerEnter2D(Collider2D collision)
     {
         int damageAmount = 10;
+                    Enemy enemy = collision.GetComponent<Enemy>();
 
         if (collision.CompareTag("Boss"))
         {
-            Destroy(gameObject);
 
+
+            if(enemy != null){
+                enemy.takeDamage(damageAmount);
+                Destroy(gameObject);
+            }
         }else if (collision.CompareTag("Enemy"))
         {
-            Enemy enemy = collision.GetComponent<Enemy>();
+
 
             if(enemy != null){
                 enemy.takeDamage(damageAmount);
