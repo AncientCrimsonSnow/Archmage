44ce4c6f2fb17cea4cb33d7ef9f8a70fababdc28
Daria Katysheva <dariakatysheva92@gmail.com>
2020-12-05 19:36:08 +0100

    Add loading screen

---
+++ ge-roguelike-game-project/Assets/Scripts/GameControllers/GameLoader.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameLoader : MonoBehaviour
+{
+    [SerializeField]
+    private Slider slider;
+    public string loadedScene;
+    [SerializeField]
+    private Text progressText;
+
+    void Start()
+    {
+        if (loadedScene != null)
+        {
+            LoadLevel(loadedScene);
+        }
+        
+    }
+
+    public void LoadLevel(string sceneName)
+    {
+        StartCoroutine(LoadAsynchronously(sceneName));
+    }
+
+    IEnumerator LoadAsynchronously(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+
+        while (!operation.isDone)
+        {
+            if(slider)
+            {
+                float progress = Mathf.Clamp01(operation.progress / .9f);
+
+                slider.value = progress;
+
+                progressText.text = progress * 100f + "%";
+
+                yield return new WaitForEndOfFrame();
+            }
+            
+        }
+    }
+}
--- ge-roguelike-game-project/Assets/Scripts/UI/MainMenu.cs
+++ ge-roguelike-game-project/Assets/Scripts/UI/MainMenu.cs
@@ -5,24 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public string newGameScene;
-
-    public GameObject continueButton;
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string loadingScene;
 
     public void NewGame()
     {
-        SceneManager.LoadScene(newGameScene);
+        SceneManager.LoadScene(loadingScene);
     }
 
     public void Exit()
